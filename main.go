63060487c6db03e68c1e14fe0ebffe85ff72c7f9
mkomatineni95 <88016991+mkomatineni95@users.noreply.github.com>
2021-12-03 14:22:21 -0500

    Update main.go

--- main.go
+++ main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
-	corev1 "k8s.io/api/core/v1"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	config, err := clientcmd.BuildConfigFromFlags("", "<kube-config-path>")
+	config, err := clientcmd.BuildConfigFromFlags("", "./kubeconfig.yaml")
 	if err != nil {
 		return
 	}
@@ -28,20 +28,20 @@ func main() {
 		fmt.Println(n.Name)
 	}
 
-	newPod := &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "test-pod",
-		},
-		Spec: corev1.PodSpec{
-			Containers: []corev1.Container{
-				{Name: "busybox", Image: "busybox:latest", Command: []string{"sleep", "100000"}},
-			},
-		},
-	}
+	// newPod := &corev1.Pod{
+	// 	ObjectMeta: metav1.ObjectMeta{
+	// 		Name: "test-pod",
+	// 	},
+	// 	Spec: corev1.PodSpec{
+	// 		Containers: []corev1.Container{
+	// 			{Name: "busybox", Image: "busybox:latest", Command: []string{"sleep", "100000"}},
+	// 		},
+	// 	},
+	// }
 
-	pod, err := c.CoreV1().Pods("default").Create(context.Background(), newPod, metav1.CreateOptions{})
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(pod)
+	// pod, err := c.CoreV1().Pods("default").Create(context.Background(), newPod, metav1.CreateOptions{})
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(pod)
 }
